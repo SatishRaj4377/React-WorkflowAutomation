597fcad1b769c6b0548f69e55ac75134bb571b25
SatishRaj4377 <158574671+SatishRaj4377@users.noreply.github.com>
2025-09-12 19:28:19 +0530

    added the logo icon in the icon.ts file and then used it in the home

--- src/assets/icons.ts
+++ src/assets/icons.ts
@@ -1,18 +1,20 @@
-import { ReactComponent as AiAgentIcon } from '../assets/images/node-icons/ai-agent.svg';
-import { ReactComponent as AzureModelIcon } from '../assets/images/node-icons/azure-model.svg';
-import { ReactComponent as ChatIcon } from '../assets/images/node-icons/chat.svg';
-import { ReactComponent as FilterIcon } from '../assets/images/node-icons/filter.svg';
-import { ReactComponent as GmailIcon } from '../assets/images/node-icons/gmail.svg';
-import { ReactComponent as GoogleDocsIcon } from '../assets/images/node-icons/google-docs.svg';
-import { ReactComponent as GoogleCalendarIcon } from '../assets/images/node-icons/google-calendar.svg';
-import { ReactComponent as GoogleSheetIcon } from '../assets/images/node-icons/google-sheet.svg';
-import { ReactComponent as HttpRequestIcon } from '../assets/images/node-icons/http-request.svg';
-import { ReactComponent as IfConditionIcon } from '../assets/images/node-icons/if-condition.svg';
-import { ReactComponent as ScheduleIcon } from '../assets/images/node-icons/schedule.svg';
-import { ReactComponent as SwitchConditionIcon } from '../assets/images/node-icons/switch-condition.svg';
-import { ReactComponent as TelegramIcon } from '../assets/images/node-icons/telegram.svg';
-import { ReactComponent as TwilioIcon } from '../assets/images/node-icons/twilio.svg';
-import { ReactComponent as WebhookIcon } from '../assets/images/node-icons/webhook.svg';
+import { ReactComponent as WorkflowLogo } from '../assets/images/svg-icons/workflow-logo.svg';
+import { ReactComponent as AiAgentIcon } from '../assets/images/svg-icons/ai-agent.svg';
+import { ReactComponent as AzureModelIcon } from '../assets/images/svg-icons/azure-model.svg';
+import { ReactComponent as ChatIcon } from '../assets/images/svg-icons/chat.svg';
+import { ReactComponent as FilterIcon } from '../assets/images/svg-icons/filter.svg';
+import { ReactComponent as GmailIcon } from '../assets/images/svg-icons/gmail.svg';
+import { ReactComponent as GoogleDocsIcon } from '../assets/images/svg-icons/google-docs.svg';
+import { ReactComponent as GoogleCalendarIcon } from '../assets/images/svg-icons/google-calendar.svg';
+import { ReactComponent as GoogleSheetIcon } from '../assets/images/svg-icons/google-sheet.svg';
+import { ReactComponent as HttpRequestIcon } from '../assets/images/svg-icons/http-request.svg';
+import { ReactComponent as IfConditionIcon } from '../assets/images/svg-icons/if-condition.svg';
+import { ReactComponent as ScheduleIcon } from '../assets/images/svg-icons/schedule.svg';
+import { ReactComponent as SwitchConditionIcon } from '../assets/images/svg-icons/switch-condition.svg';
+import { ReactComponent as TelegramIcon } from '../assets/images/svg-icons/telegram.svg';
+import { ReactComponent as TwilioIcon } from '../assets/images/svg-icons/twilio.svg';
+import { ReactComponent as WebhookIcon } from '../assets/images/svg-icons/webhook.svg';
+
 import ApiIntegrationImage from '../assets/images/template-images/api-integration.jpg';
 import DataProcessingImage from '../assets/images/template-images/data-processing.jpg';
 import DefaultImageImage from '../assets/images/template-images/default-image.jpg';
@@ -20,6 +22,7 @@ import EmailAutomationImage from '../assets/images/template-images/email-automat
 import NotificationSystemImage from '../assets/images/template-images/notification-system.jpg';
 
 export const NodeIcons = {
+  WorkflowLogo,
   AiAgentIcon,
   AzureModelIcon,
   ChatIcon,
